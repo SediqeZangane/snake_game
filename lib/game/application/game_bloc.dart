fc82c396a093e71536634980ecd15a9296a23d3c
sedighe zanganeh <se.zanganeh@yahoo.com>
2023-11-22 23:07:16 +0100

    set first position for snake and seed

--- lib/game/application/game_bloc.dart
+++ lib/game/application/game_bloc.dart
@@ -1,15 +1,30 @@
+import 'dart:math';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:snake_game/game/application/game_event.dart';
 import 'package:snake_game/game/application/game_state.dart';
+import 'package:snake_game/shared/cell_type.dart';
 
 const boardSize = 16;
 
 class GameBloc extends Bloc<GameEvent, GameSate> {
   GameBloc() : super(GameSate.init()) {
     on<GameEvent>((event, emit) {
-      if (event is GameInitEvent) {}
+      if (event is GameInitEvent) {
+        state.board[randomInt()][randomInt()] = CellType.seed;
+        final rowHead=randomInt();
+        final columnHead=randomInt();
+        state.board[rowHead][columnHead] = CellType.head;
+        state.board[rowHead][columnHead-1] = CellType.tail;
+
+        emit(state);
+      }
 
       if (event is GameUpEvent) {}
     });
+  }
+
+  int randomInt() {
+    return Random().nextInt(boardSize-1);
   }
 }
